6f85242b281989463cb982c5e461c0f5817f0f35
ashwini381 <ashwiniganu38@gmail.com>
2021-08-05 14:15:08 +0000

    m3 new match commit
    m4 new match commit space is added

---
+++ Cal.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Cal
+{
+        public static void main(String args[])
+        {
+int a,b,sum;
+Scanner scan=new Scanner(System.in);
+System.out.println("enter a value");
+a=scan.nextInt();
+System.out.println("Enter b value");
+b=scan.nextInt();
+sum = a+b;
+System.out.println("The sum of a and b i:" + sum);
+        }
+}
